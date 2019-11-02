7cf3923856c827df430eed660c17335f06cd0d4f
Kayro Mizael <kayromsa@gmail.com>
2019-11-02 18:55:59 -0300

    initial commit for seasson model and controller with mongodb

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,9 @@
+const User = require('../models/User');
+// index, show, store, update, destroy
+
+module.exports = {
+    store(requisition, response){
+        const { email }  = requisition.body.email;
+
+    }
+};
